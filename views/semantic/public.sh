c71fa9e4dcbdd0665104a918c78a0c9d933b7090
Jeni Grant <git@jenigrant.com>
2017-01-10 00:28:42 -0500

    inital refactor complete

---
+++ views/semantic/public.sh
@@ -0,0 +1,6 @@
+#/bin/bash/
+
+cp dist/*.css ../../public/stylesheets
+cp dist/*.js ../../public/javascripts
+cp dist/components/*.css ../../public/stylesheets/components/
+cp dist/components/*.js ../../public/javascripts/components/

ee94c1c574842a3fafd031603433e867bf4c1ad0
Jeni Grant <git@jenigrant.com>
2017-01-17 20:21:17 -0500

    Removed unnecessary files from previous versions Removed need for template engine Cleaned up directory structure Streamlined build processes Dockerized just for the hell of it

--- views/semantic/public.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash/
-
-cp dist/*.css ../../public/stylesheets
-cp dist/*.js ../../public/javascripts
-cp dist/components/*.css ../../public/stylesheets/components/
-cp dist/components/*.js ../../public/javascripts/components/
